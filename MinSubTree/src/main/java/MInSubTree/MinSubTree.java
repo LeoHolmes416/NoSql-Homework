fced5634222e60190a3bdfd16cf9ff9345058e0b
LeoHolmes416 <59080906+LeoHolmes416@users.noreply.github.com>
2020-12-09 20:11:01 +0800

    Signed-off-by: LeoHolmes416 <59080906+LeoHolmes416@users.noreply.github.com>

---
+++ MinSubTree/src/main/java/MInSubTree/MinSubTree.java
@@ -0,0 +1,23 @@
+package MInSubTree;
+
+public class MinSubTree {
+    private int minSum;
+    private TreeNode minRoot;
+    public TreeNode findSubTree(TreeNode root){
+        minSum = Integer.MAX_VALUE;
+        minRoot = null;
+        getSum(root);
+        return minRoot;
+    }
+    private int getSum(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        int sum = getSum(root.left)+getSum(root.right)+root.val;
+        if(sum<minSum) {
+            minSum = sum;
+            minRoot = root;
+        }
+        return sum;
+    }
+}
---
+++ MinSubTree/src/main/java/MInSubTree/TreeNode.java
@@ -0,0 +1,10 @@
+package MInSubTree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x){
+        val =x;
+    }
+}
